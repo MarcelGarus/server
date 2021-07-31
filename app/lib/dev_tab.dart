3173ab4b1d4648ad519edcd21bd87bec3ea2847d
Marcel Garus <marcel.garus@gmail.com>
2021-07-31 21:41:21 +0200

    Improve user agent diagram

--- app/lib/dev_tab.dart
+++ app/lib/dev_tab.dart
@@ -7,6 +7,7 @@ import 'package:dartx/dartx.dart';
 import 'package:tuple/tuple.dart';
 
 import 'api.dart' as api;
+import 'utils.dart';
 
 class DevTab extends StatelessWidget {
   const DevTab({Key? key}) : super(key: key);
@@ -67,112 +68,91 @@ class UserAgentGraph extends StatelessWidget {
   }
 
   Widget _buildDiagram(
-      BuildContext context, Map<DateTime, Map<String, int>> data) {
-    // Relatively consistent requests should be shown at the bottom.
-    // For example, StatusCake tests or Googlebot crawls should be at
-    // the bottom of the chart so that more varying user agents are on
-    // the top and easier to see. That's why we calculate how much each
-    // user agent distribution deviates from its mean.
-    final userAgents = data.values.expand((map) => map.keys).toSet();
-    final averages = <String, double>{
-      for (final userAgent in userAgents)
-        userAgent: data.entries
-                .map((entry) => entry.value[userAgent] ?? 0)
-                .reduce((a, b) => a + b) /
-            data.length,
-    };
-    final deviations = <String, double>{
-      for (final userAgent in userAgents)
-        userAgent: () {
-          final average = averages[userAgent]!;
-          return data.entries
-              .map((entry) => ((entry.value[userAgent] ?? 0) - average).abs())
-              .reduce((a, b) => a + b);
-        }(),
-    };
-    final userAgentsSortedByConsistency = userAgents.toList()
-      ..sort((a, b) {
-        final difference = deviations[a]! - deviations[b]!;
-        if (difference < 0) return -1;
-        if (difference > 0) return 1;
-        return 0;
-      });
+    BuildContext context,
+    Map<DateTime, Map<String, int>> data,
+  ) {
+    // In the diagram, more constant sources of traffic should be displayed at
+    // the bottom. These are the basic things to display:
+    // - 3: other visits
+    // - 2: debugging, like this app or Postman
+    // - 1: crawlers, bots, spiders
+    // - 0: StatusCake uptime monitoring
+    int topness(String userAgent) {
+      if (userAgent.contains('StatusCake')) return 0;
+      if (userAgent.toLowerCase().contains('bot')) return 1;
+      if (userAgent.toLowerCase().contains('research')) return 1;
+      if (userAgent.toLowerCase().contains('inspect')) return 1;
+      if (userAgent.contains('python')) return 1;
+      if (userAgent.contains('dart')) return 1;
+      if (userAgent.contains('CompanionApp')) return 2;
+      if (userAgent.contains('PostmanRuntime')) return 2;
+      return 3;
+    }
 
-    final botColors = [
-      for (var i = 0.0; i < 1.0; i += 0.1)
-        Color.lerp(Colors.purple, Color(0xfff97191), i)!
-    ];
-    final otherColors = [Colors.amber, Colors.orange];
-    final userAgentNamesAndColors = <String, Tuple2<String?, Color>>{
-      for (final userAgent in userAgents)
-        userAgent: () {
-          if (userAgent.contains('StatusCake')) {
-            return Tuple2<String?, Color>('StatusCake', Color(0xff5af1c8));
-          }
-          if (userAgent.contains('PostmanRuntime')) {
-            return Tuple2<String?, Color>('Postman', Color(0xffff6c37));
-          }
-          if (userAgent == 'CompanionApp') {
-            return Tuple2<String?, Color>(
-                '(this companion app)', Color(0xff999999));
-          }
-          if (userAgent.toLowerCase().contains('bot')) {
-            return Tuple2<String?, Color>(
-              null,
-              botColors.isEmpty ? Colors.pink : botColors.removeAt(0),
-            );
-          }
+    final userAgentInfos = data.values
+        .expand((map) => map.keys)
+        .toSet()
+        .map((userAgent) => UserAgentInfo.from(userAgent))
+        .sortedBy((userAgent) => userAgent.type.index);
 
-          final random = Random(userAgent.hashCode);
-          return Tuple2<String?, Color>(
-            null,
-            otherColors.isNotEmpty
-                ? otherColors.removeAt(0)
-                : Color.fromARGB(255, random.nextInt(255), random.nextInt(255),
-                    random.nextInt(255)),
-          );
-        }(),
+    final botColors = [Colors.purple, Color(0xfff97191)].cycle().iterator;
+    final humanColors = [Colors.amber, Colors.orange].cycle().iterator;
+    Color colorFor(UserAgentInfo info) {
+      switch (info.type) {
+        case UserAgentType.uptimeMonitoring:
+          return Color(0xff5af1c8);
+        case UserAgentType.bot:
+          return botColors.next();
+        case UserAgentType.debugging:
+          if (info.userAgent.toLowerCase().contains('postmanruntime'))
+            return Color(0xffff6c37);
+          if (info.userAgent.toLowerCase().contains('companionapp'))
+            return Color(0xff444444);
+          return Colors.pink; // Unknown debugging user agent.
+        case UserAgentType.human:
+          return humanColors.next();
+      }
+    }
+
+    final colors = {
+      for (final info in userAgentInfos) info.userAgent: colorFor(info)
     };
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SizedBox(
           height: 300,
           child: SfCartesianChart(
             series: <ChartSeries>[
-              for (final userAgent in userAgentsSortedByConsistency)
+              for (final info in userAgentInfos)
                 StackedColumnSeries<MapEntry<DateTime, Map<String, int>>, int>(
                   dataSource: data.entries.toList(),
                   xValueMapper: (report, _) =>
                       report.key.millisecondsSinceEpoch,
-                  yValueMapper: (report, _) => report.value[userAgent],
-                  color: userAgentNamesAndColors[userAgent]!.item2,
+                  yValueMapper: (report, _) => report.value[info.userAgent],
+                  color: colors[info.userAgent]!,
                 ),
             ],
             primaryXAxis: CategoryAxis(name: 'time'),
           ),
         ),
-        for (final userAgent in userAgents) ...[
-          SizedBox(height: 8),
-          Row(
-            children: [
-              Container(
-                width: 16,
-                height: 16,
-                decoration: BoxDecoration(
-                  color: userAgentNamesAndColors[userAgent]!.item2,
-                  borderRadius: BorderRadius.circular(8),
+        Wrap(
+          spacing: 8,
+          runSpacing: 8,
+          children: [
+            for (final info in userAgentInfos.reversed)
+              ActionChip(
+                backgroundColor: colors[info.userAgent]!,
+                label: Text(
+                  info.simpleName ?? info.userAgent,
+                  style: TextStyle(
+                      color: colors[info.userAgent]!.highEmphasisOnColor),
                 ),
+                onPressed: () => context.showSimpleSnackBar(info.userAgent),
               ),
-              SizedBox(width: 16),
-              Expanded(
-                child: Text(
-                  userAgentNamesAndColors[userAgent]!.item1 ?? userAgent,
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ],
     );
   }
